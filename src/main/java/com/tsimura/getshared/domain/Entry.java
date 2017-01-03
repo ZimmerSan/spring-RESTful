9cd6a195a6bfe0045002668069f96478f62a46ad
Paul Tsimura <paultsimura@gmail.com>
2017-01-03 15:48:50 +0200

    Added Lombok

--- src/main/java/com/tsimura/getshared/domain/Entry.java
+++ src/main/java/com/tsimura/getshared/domain/Entry.java
@@ -1,9 +1,12 @@
 package com.tsimura.getshared.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
+@Data
 @Entity
 @Table(name = "entries")
 public class Entry implements Serializable {
@@ -20,42 +23,4 @@ public class Entry implements Serializable {
 
     @Column(name = "modified")
     private Date modified;
-
-    @Override
-    public String toString() {
-        return "Entry{" + id + ", title='" + title + '\'' + '}';
-    }
-
-    // Getters and setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Date getCreated() {
-        return created;
-    }
-
-    public void setCreated(Date created) {
-        this.created = created;
-    }
-
-    public Date getModified() {
-        return modified;
-    }
-
-    public void setModified(Date modified) {
-        this.modified = modified;
-    }
 }
